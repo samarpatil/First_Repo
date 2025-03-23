95987e0fd0a3b36c660601ff4b9c346db799f3f2
Samar Patil <patilsamar2304@gmail.com>
2025-03-23 10:33:03 +0530

    First file commited

---
+++ fizzbuzz.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int k;
+        cin>>k;
+        string a;
+        if(k==1)
+        {
+            a = "YES";
+        }
+        else if()
+    }
+}
